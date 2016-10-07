91e8bbb41463733f78ae18a9b05f1ae3f0072cac
star-yar <vestnik.yar@gmail.com>
2016-10-07 18:52:33 +0300

    DTMF file add

--- 01-sound/solutions/StYaroslav/py_music.py
+++ 01-sound/solutions/StYaroslav/py_music.py
@@ -1,13 +1,26 @@
 import wave, random, struct, math
 
-noise_file = open('noise.raw', 'wb')
+def gener(i, Gz):
 
-k=425
+        AMP=63.5
+        diskr=44100
+        
+        value = AMP*math.sin(2*math.pi*i*Gz1/diskr)
 
-for i in range(0, 1000):
-        value = 2*math.sin(2*math.pi*i*k/8000)
-        #print(str(value))
-        value=int(value)
-        packed_value = struct.pack('b', value)
-        noise_file.write((packed_value))
-noise_file.close()
+        return value
+
+def plus_nota(Gz1, Gz2):
+        noise_file = open('noise.raw', 'ab')
+        for i in range(0, 11025):
+                value = gener(i, Gz1) + gener(i, Gz2)
+                #print(str(value))
+                value=int(value)
+                packed_value = struct.pack('b', value)
+                noise_file.write((packed_value))
+        noise_file.close()
+
+for Gz1 in (697, 770, 852, 941):
+        for Gz2 in (1209, 1336, 1477):
+                plus_nota(Gz1,Gz2)
+                plus_nota(Gz1,Gz2)
+                plus_nota(Gz1,Gz2)
