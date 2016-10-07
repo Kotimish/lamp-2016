f04779764003d64a210d4dd4c86b6d7e0458630b
IliaOv <ilya.ovchinniko@gmail.com>
2016-10-07 12:59:21 +0300

    merged branch

--- 01-sound/assignments/ex1_freq.js
+++ 01-sound/assignments/ex1_freq.js
@@ -1,12 +1,8 @@
 var i, j, s, value;
-<<<<<<< HEAD
 
 var AMP = 10; 
 for (i=0; i<8000; i++) {
-=======
-var AMP = 10;
-for (i=0; i<10; i++) {
->>>>>>> 21e99d4a4b1a047fc2a35c3810552e1eaf42f1f8
+
   value = AMP * Math.sin(
     2*Math.PI*i/10
   );
