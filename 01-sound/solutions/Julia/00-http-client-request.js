29f15f915dce698274bf41c948d740a76e3f97a6
JuliaKonochkina <u.k.v.15011973@yandex.ru>
2016-10-11 11:41:08 +0300

    hjvv

---
+++ 01-sound/solutions/Julia/00-http-client-request.js
@@ -0,0 +1,41 @@
+﻿/**
+ * Задание
+ * =======
+ *
+ * С использованием примера ниже, получить HTML страницу с прогнозом погоды по городу Москва
+ *
+ *
+ */
+
+
+
+'use strict';
+
+
+
+var request = require('request');
+var cheerio = require("cheerio");
+var url = 'https://www.gismeteo.ru/city/daily/4368/';
+var result = '',
+i=0;
+
+
+
+request(url, function (error, response, body) {
+  
+  if (!error) {
+
+   var $ = cheerio.load(body),
+	temperature = $("dd.value.m_temp.c").html(),
+	tempArray = temperature.split("")
+while (tempArray[i] !== '<') {
+	result = result + tempArray[i];
+	i++;
+}
+
+
+ console.log("Температура " + result + " градусов по Цельсию ");
+    } else {
+        console.log("Произошла ошибка: " + error);
+    }
+});
