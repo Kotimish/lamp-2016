53ac266433cbc080b831f30d4e4c01963cd34db2
SvKostenko <s.krast@mail.ru>
2016-10-14 13:45:05 +0300

    privet

---
+++ 300-mocha-chai/solutions/KostenKonochkina/tests/lib/math.js
@@ -0,0 +1,66 @@
+/* globals describe, before, beforeEach, after, afterEach, it */
+
+'use strict';
+
+var chai = require('chai');
+//var assert = chai.assert;
+var expect = chai.expect;
+//var should = chai.should();
+
+
+// declaring global vars
+var math;
+
+
+describe('math', function() {
+
+  before('before', function() {
+    // initializing common test data
+    math = require('../../lib/math.js');
+  });
+
+  beforeEach('beforeEach', function() {
+  });
+
+  afterEach('afterEach', function() {
+  });
+
+  after('after', function() {
+  });
+
+
+  describe('square', function() {
+
+    it('is a function', function () {
+
+      console.log(math);
+      expect(math.square(2)).to.equal(4);
+
+    });
+	it('is a function', function () {
+
+      console.log(math);
+      expect(math.square(3)).to.equal(9);
+
+    });
+it('is a function', function () {
+
+      console.log(math);
+      expect(math.square(27)).to.equal(729);
+
+    });
+
+it('is a function', function () {
+
+      console.log(math);
+      expect(math.square(-27)).to.equal(729);
+
+    });
+
+
+  });
+
+
+});
+
+
